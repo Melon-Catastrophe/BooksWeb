ee957691f99e6305c868ee25abdb576382ab88b6
Melon-Catastrophe <Melon-Catastrophe@WII>
2025-02-25 15:54:59 -0700

    Dynamic Details - Created Details.cshtml to allow for dynamic viewing of the details of books. - Books are found by the Title, not ID. In the future, find the book by ID.

--- BooksWeb/Controllers/BookController.cs
+++ BooksWeb/Controllers/BookController.cs
@@ -11,6 +11,9 @@ public class BookController : Controller
     private readonly MockBookRepository booksRepository = new();
     private readonly BookMapper mapper = new();
     
+    /// <summary>
+    /// The landing page of the web app. Displays a table of all the books in the database.
+    /// </summary>
     public IActionResult Index()
     {
         List<Book> books = booksRepository.GetAllBooks();
@@ -22,5 +25,54 @@ public class BookController : Controller
         }
 
         return View(viewBooks);
+    }
+
+    /// <summary>
+    /// Shows details of a Book. Used to demonstrate dynamic page navigation.
+    /// </summary>
+    /// <param name="title">The title of the book.</param>
+    public IActionResult Details(string? title)
+    {
+        if (title == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        List<Book> books = booksRepository.GetAllBooks();
+        BookViewModel? foundBook = FindBookFromTitle(books, title);
+
+        if (foundBook != null)
+        {
+            return View(foundBook);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// Searches the list of Books provided for the given title. 
+    /// Returns the BookViewModel if its title is found, or null otherwise.
+    /// </summary>
+    /// <remarks>
+    /// Ideally, this function would find the book based on its ID. However,
+    /// the Index.cshtml file receives the books via the BookViewModel class,
+    /// which does not contain the book's ID. If given more time, a better solution 
+    /// would be found.
+    /// </remarks>
+    /// <param name="books">The list of all books.</param>
+    /// <param name="title">The title of the book.</param>
+    private BookViewModel? FindBookFromTitle(List<Book> books, string title)
+    {
+        foreach (Book book in books)
+        {
+            if (book.Title == title)
+            {
+                return mapper.ToViewModel(book);
+            }
+        }
+
+        return null;
     }
 }
